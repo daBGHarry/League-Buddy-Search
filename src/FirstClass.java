731c16086216498ad2eb1c573540bf0b28e1b938
Harrison Ianatchkov <harrison1993@gmail.com>
2015-08-24 17:35:57 -0400

    Example program that scrapes off the hrefs from the wikipedia page

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,27 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+
 
 public class FirstClass {
 	
-	
+	public static void main(String[] args) {
+		try {
+			Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+			Elements links = doc.select("a");
+			
+			for (Element e : links) {
+				System.out.println(e.attr("abs:href"));
+			}
+		}
+		catch (IOException e) {
+			
+		}
+	}
 
 }
